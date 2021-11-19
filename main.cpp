814b12866eb69884c1d2fdf19b54f27672158c59
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2021-11-19 02:07:43 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,21 @@
+//
+// Creado por Alfredo Gómez Mendoza A01704189 el 10/11/2021.
+//
 #include <iostream>
 #include <string>
-#include "Perro.h"
 #include "Voluntario.h"
-#include "Adoptante.h"
 using namespace std;
 int main(){
+    //Se crea un refugio con datos iniciales
+    Refugio huellas_seguras("Huellitas Seguras",30,32,70,90,90);//en este
+        //constructor se define el número máximo de perros y voluntarios
 
     //Se crea un voluntario con datos iniciales
-    Voluntario gabriel ("Gabriel", 22,0,40,442343434);
+    Voluntario gabriel ("Gabriel", 22,0,40,442343434,huellas_seguras);
     //en caso que el voluntario haya cambiado su número de teléfono
     gabriel.set_tel(442345678);
+    //agregamos a gabriel al contador de voluntarios de huellas seguras
+    gabriel.anadir_voluntario();
     //en caso que el voluntario haya cumplido años
     gabriel.set_edad(23);
     //en caso que se quiera cambiar el nombre
@@ -22,22 +28,17 @@ int main(){
     //colaborador experimentado
     gabriel.colab_exp();
 
-    //se crea un adoptante
-    Adoptante benjamin("Carrizales", 22, "Reinaldo", 0,0);
-    //info determinada por usuario
-    //Mediante los setters, modificaremos los atributos del adoptante
-    benjamin.set_nombre("Benjamin Carrizales"); benjamin.set_edad(23);benjamin.set_perro("Rodo");
-    //El usuario determinara que cambiar
-    benjamin.set_alimento(1);//El usuario que use el programa checa que el adoptante le proporcione alimento
-    benjamin.set_veterinario(1);//El usuario que use el programa checa que el adoptante pueda llevar al perro al veterinario
-    //Esto lo define mediante 1 o 0, siendo 0 que no cumple y 1 que si.
-    cout<<benjamin.get_nombre()<<" de "<<benjamin.get_edad()<<" quiere adoptar a "<<benjamin.get_perro()<<endl;
-    cout<<"¿Puede alimentar al perro?: "<<benjamin.get_alimento()<<endl; cout<<"¿Puede llevar al perro al veterinario?: "<<benjamin.get_iralvet()<<endl;
-    cout<<benjamin.adoptar()<<endl; //Si es correcto todo el proceso, el usuario puede adoptar
-    //Con esto podemos buscar la info de los perros y enseñarselas al usuario
-    //pero antes el usuario puede actualizarla en caso que no lo esté
+    //dummy definido para poder crear a rodo (el perro o nuestro objeto de clase perro)
+    Adoptante dummy;
     Perro rodo("Cocker", 2, .3,0, "Jugueton",//creamos al perro
-               "Con ninos y perros",0, 1, 0,0); //info determinada
+               "Con ninos y perros",0, 1, 0,0,dummy,huellas_seguras); //info determinada
+    //setter de adoptante
+    rodo.anadir_perro(); //se usa esta función cada vez que se crea un perro, así se contabiliza en el atributo del
+    //objeto refugio
+    rodo.set_adoptante(dummy);
+    //se crea un adoptante
+    rodo.crear_adoptante();
+    rodo.adoptar();
     Perro rocky; //creamos otro perro
     //Mediante los setters, modificaremos los atributos del perro, (actualizamos la información)
     rodo.set_edad(15); //cambiamos la información determinada del perro rodo
@@ -49,7 +50,6 @@ int main(){
     rodo.set_enfermo(0);
     rodo.set_vacunado(1);
     rodo.set_esterilizado(1);
-    rodo.adoptado(1);
 
  //Sacamos toda la información del perro que acabamos de cambiar para enseñarsela al usuario
     cout << "Raza:"<<rodo.get_raza()<<"\n";
@@ -62,7 +62,21 @@ int main(){
     cout<<"¿Esta vacunado?:"<<rodo.get_vacunado()<<"\n";
     cout<<"¿Esta esterilizado?:"<<rodo.get_esterilizado()<<"\n";
     cout<<"Estatus:"<<rodo.get_estatus()<<"\n";
-    rodo.~Perro();
+
+    //informacion final antes de cerrar el programa
+    cout<<"¿Se ocupan camas?"<<endl;
+    cout<<huellas_seguras.camas_necesarias()<<endl;
+    cout<<"¿Hay espacio para más perros?"<<endl;
+    cout<<huellas_seguras.perros_admitidos()<<endl;
+    cout<<"¿Se requiere más voluntarios?"<<endl;
+    cout<<huellas_seguras.staff_necesario()<<endl;
+
+    cout<<"¿Deseas destruir la información de Rodo? Verdadero=1, Falso=0"<<endl;
+    bool opcion;
+    cin>>opcion;
+    if (opcion==true){
+        rodo.~Perro();
+    }
 
     return 0;
 }
