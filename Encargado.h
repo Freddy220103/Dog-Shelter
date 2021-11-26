7989c078837f6c50c06b9fc88794738c02c5bc19
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2021-11-26 03:26:53 -0600

    Update Encargado.h

--- Encargado.h
+++ Encargado.h
@@ -1,12 +1,38 @@
 //
 // Creado por Alfredo Gómez Mendoza A01704189 el 18/11/2021.
 //Parte de herencia
-class Encargado :: public Voluntario{
-    private:
-        int dinero_total;
-    public:
-        Encargado(){};
-        string pagar_impuestos(int,int);
-        int ganar_dinero(int);
-        int gastar_dinero(int);
-    };
+#ifndef ENCARGADO_H_
+#define ENCARGADO_H_
+#include "Voluntario.h"
+class Encargado : public Voluntario {
+public:
+    Encargado() : Voluntario() {};
+
+
+    Encargado(string nombre, int edad, int colaborador_experimentado, int horas_cumplidas, int telefono,
+              Refugio refugio) : Voluntario(nombre, edad, colaborador_experimentado, horas_cumplidas, telefono,
+                                            refugio) {};
+
+    string pagar_deudas();
+/*Encargado (string nombre, int edad, int colaborador_experimentado,int horas_cumplidas, int telefono):
+            Voluntario(nom, name, age, experience, position){};
+    Voluntario(): nombre("Voluntario"),edad(16), colaborador_experimentado(0), horas_cumplidas(0), telefono(442324435){};*/
+};
+
+    string Encargado::pagar_deudas() {
+        bool cumplir;
+        string text;
+        text="";
+        cout<<"¿El encargado ya pago las deudas?"<<" Verdadero=1/Falso=0"<<endl;
+        cin>>cumplir;
+        cout<<"";
+        if(cumplir==1){
+            text="El encargado "+get_nombre()+ " ha pagado las deudas correspondidas";
+        }
+        else{
+            text="El encargado "+get_nombre()+ " no ha pagado las deudas correspondidas";
+        }
+        return text;
+    }
+#endif
+
