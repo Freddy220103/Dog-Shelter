4198630ba8f94552c6ab2bd76b009dbef2787710
Fredy Gómez <44040347+Freddy220103@users.noreply.github.com>
2021-12-03 01:53:22 -0600

    Update Adoptante.h

--- Adoptante.h
+++ Adoptante.h
@@ -1,5 +1,6 @@
 //
 // Creado por Alfredo Gómez Mendoza A01704189 el 10/11/2021.
+// Actualizado el 02/12/21
 //
 #ifndef ADOPTANTE_H_
 #define ADOPTANTE_H_
@@ -8,8 +9,10 @@ using namespace std;
 
 class Adoptante{
 private:
-    string nombre;
-    int edad;
+
+    //atributos privados
+    string nombre;//nombre del adoptante
+    int edad; //edad del adoptante
     string perro_adoptado; //nombre del perro que será adoptado
 
 
@@ -17,8 +20,11 @@ public:
     bool proporcionar_alimento; //variable dónde se guarda si el adoptante puede proporcionarle alimento al perro
     bool ir_alveterinario; //variable dónde se guarda si el adoptante puede llevar al veterinario al perro en cuestión
     bool cumple_criterio; //Variable que determina si el adoptante es capaz de tener al perro, se pone 1 para true, 0 para false
-    Adoptante(): nombre("Adoptante"),edad(20), perro_adoptado("Perro adoptado"), proporcionar_alimento(false), ir_alveterinario(false),cumple_criterio(false){};
+
     //constructores
+    Adoptante(): nombre("Adoptante"),edad(20), perro_adoptado("Perro adoptado"), proporcionar_alimento(false),
+    ir_alveterinario(false),cumple_criterio(false){};
+
     Adoptante(string nom, int eda, string perro, bool alimento, bool vet):
             nombre(nom), edad(eda), perro_adoptado(perro), proporcionar_alimento(alimento), ir_alveterinario(vet){};
 
@@ -33,15 +39,24 @@ public:
     //setters de adoptante
     void set_nombre(string);
     void set_edad(int);
-    void set_perro(string);
     void set_alimento(bool);
     void set_veterinario(bool);
     void cumple_criterios();
 
-    //Método que checa si se puede adoptar
-    string adoptar();
+    //Métodos únicos
+
+    string adoptar();//Método que checa si se puede adoptar
+
+    void adoptante_info(); /*mediante este método, es más fácil checar la información del adoptante, se genera
+                            un resumen en la consola*/
+
+    void cambiarinfo(); /*se cambia la informacion mediante el uso de setters, se genera informacion en la consola
+                        para que el usuario pueda meter su informacion de una manera más sencilla*/
 };
 
+//definicion de metodos
+
+//getters de adoptante
 string Adoptante::get_nombre(){
     return nombre;
 }
@@ -64,16 +79,14 @@ bool Adoptante::get_cumple() {
 
 }
 
+
+//setters de adoptante
 void Adoptante::set_nombre(string nom){
     nombre = nom;
 }
 
 void Adoptante::set_edad(int eda){
     edad = eda;
-}
-
-void Adoptante::set_perro(string perro){
-    perro_adoptado=perro;
 }
 
 void Adoptante::set_alimento(bool alimento) {
@@ -83,6 +96,8 @@ void Adoptante::set_alimento(bool alimento) {
 void Adoptante::set_veterinario(bool vet) {
     ir_alveterinario=vet;
 }
+
+//Métodos especiales
 void Adoptante::cumple_criterios( ){
     if (proporcionar_alimento&&ir_alveterinario==true){
         cumple_criterio=true;
@@ -91,10 +106,18 @@ void Adoptante::cumple_criterios( ){
         cumple_criterio=false;
     }
 }
+
+//METODOS UNICOS
 string Adoptante::adoptar(){
 
+    //Regresamos un string debido a que no nos sirve regresar el valor booleano de cumple_criterio
+
+    //generamos una variable que será regresada mediante el return
     string adopcion;
     adopcion="";
+
+    //mediante un if statement checa si la variable cumple criterios es verdadera o falsa
+    //con la decision anterior tomada por el if statement se cambia el texto
     if (cumple_criterio==false){
         adopcion="La adopción no puede realizarse debido a que no se cumplen los requisitos";
     }
@@ -102,8 +125,66 @@ string Adoptante::adoptar(){
         adopcion="La adopción no puede realizarse debido a que el adoptante es menor de edad";
     }
     else {
-        adopcion = "El perro ha sido adoptado con éxito";
+        adopcion = "El perro ha sido adoptado con exito";
     }
+
+    //se regresa el texto
     return adopcion;
 }
+
+void Adoptante::cambiarinfo(){
+    bool cambiarinfo;
+    cambiarinfo=0;
+    cout << "¿Desea cambiar la informacion del adoptante: " <<
+         get_nombre() << " ? Verdadero=1/Falso=0" << endl;
+
+    //se actualiza la variable de la cual depende el if statement
+    cin >> cambiarinfo;
+
+    if (cambiarinfo == 1) {
+
+        //variables que nos indicarán si se quiere cambiar la info
+        bool cambiaredad, cambiarnombre;
+        cambiaredad = 0;
+        cambiarnombre = 0;
+
+
+        //variables dónde entrará la información para luego ser mandadas a su setter respectivo.
+        string nombre;
+        int edad;
+        nombre = "";
+        edad = 18;
+
+
+        cout << "¿Desea cambiar el nombre del adoptante? Verdadero=1/Falso=0" << endl;
+        cin >> cambiarnombre;
+
+        //en caso que se quiera cambiar el nombre
+        if (cambiarnombre == 1) {
+            cout << "¿Cual seria el nuevo nombre?" << endl;
+            cin >> nombre;
+            set_nombre(nombre);
+        }
+
+        cout << "¿Desea cambiar la edad del adoptante? Verdadero=1/Falso=0" << endl;
+        cin >> cambiaredad;
+
+        //en caso que se quiera cambiar la edad
+        if (cambiaredad == 1) {
+            cout << "¿Cual seria la nueva edad?" << endl;
+            cin >> edad;
+            set_edad(edad);
+        }
+
+    }
+}
+
+//resumen de la información de adoptante mediante el uso de console communication
+void Adoptante::adoptante_info() {
+    cout << "\nNombre adoptante: " << get_nombre() << endl;
+    cout << "Edad: " << get_edad() << endl;
+    cout <<"Perro designado a adoptar: "<<get_perro()<<endl;
+    cout<<"\n";
+}
+
 #endif
